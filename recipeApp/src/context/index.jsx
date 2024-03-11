becfe704eeb583cccbacbd8ed00c0fdbf1ab3e46
Rocío Hernández <rocioflauta0709@gmail.com>
2024-03-11 05:46:28 +0300

    project 1 done

--- recipeApp/src/context/index.jsx
+++ recipeApp/src/context/index.jsx
@@ -1,37 +1,69 @@
-import { createContext, useState } from 'react'
-
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const GlobalContext = createContext(null);
+
 export default function GlobalState({ children }) {
+  const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+  const [recipeDetailsData, setRecipeDetailsData] = useState(null);
+  const [favoritesList, setFavoritesList] = useState([])
 
-    const [searchParam, setSearchParam] = useState("")
-    const [loading, setLoading] = useState(false);
-    const [recipeList, setRecipeList] = useState([]);
-    const [recipeDetailsData, setRecipeDetailsData] = useState(null)
+  const navigate = useNavigate()
 
-    async function handleSubmit(event) {
-        event.preventDefault()
-        try {
-            const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`)
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+      );
 
-            const data = await res.json();
-            console.log(data);
-            if (data?.data?.recipes) {
-                setRecipeList(data?.data?.recipes)
-                setLoading(false)
-                setSearchParam("")
-            }
+      const data = await res.json();
+      if (data?.data?.recipes) {
+        setRecipeList(data?.data?.recipes);
+        setLoading(false);
+        setSearchParam("");
+        navigate('/')
+      }
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+      setSearchParam("");
+    }
+  }
 
+  function handleAddToFavorite(getCurrentItem){
+    console.log(getCurrentItem);
+    let cpyFavoritesList = [...favoritesList];
+    const index = cpyFavoritesList.findIndex(item=> item.id === getCurrentItem.id)
 
-        } catch (err) {
-            console.error(err);
-            setLoading(false)
-            setSearchParam("")
-        }
+    if(index === -1) {
+      cpyFavoritesList.push(getCurrentItem)
+    } else {
+      cpyFavoritesList.splice(index)
     }
 
-    console.log(loading, recipeList)
+    setFavoritesList(cpyFavoritesList)
+  }
 
-    return (<GlobalContext.Provider value={{ searchParam, loading, recipeList, setSearchParam, handleSubmit, recipeDetailsData, setRecipeDetailsData }}>     {children}
-    </GlobalContext.Provider>)
+  console.log(favoritesList, 'favoritesList');
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        searchParam,
+        loading,
+        recipeList,
+        setSearchParam,
+        handleSubmit,
+        recipeDetailsData,
+        setRecipeDetailsData,
+        handleAddToFavorite,
+        favoritesList
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
