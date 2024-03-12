efd3c12e59cf9130b0a96b16f1cf44ce517dafc1
Rocío Hernández <rocioflauta0709@gmail.com>
2024-03-12 05:20:59 +0300

    updates

--- recipeApp/src/App.jsx
+++ recipeApp/src/App.jsx
@@ -10,9 +10,9 @@ export default function App() {
       <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} ></Route>
-          <Route path="/favorites" element={<Favorites />} ></Route>
-          <Route path="/recipe-item/:id" element={<Details />} ></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/recipe-item/:id" element={<Details />} />
           
         </Routes>
       </div>
