929d32ada68e7b5118d8d45ab71697e0af0a634b
Rocío Hernández <rocioflauta0709@gmail.com>
2024-03-16 02:11:15 +0300

    project completed

--- movieApp/src/App.jsx
+++ movieApp/src/App.jsx
@@ -17,12 +17,13 @@ const movie1 = {
 
 export default function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   }
 
   useEffect(() => {
@@ -37,17 +38,29 @@ export default function App() {
         <input 
         type="text"
         placeholder="Search for movies"
-        value="Superman"
-        onChange={() => {}} />
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)} 
+        />
         <img src={SearchIcon} 
         alt="search" 
-        onClick={() => {}}
+        onClick={() => searchMovies(searchTerm)}
         />
       </div>
 
-      <div className="container">
-        <MovieCard movie1={movie1} />
-      </div>
+      { movies?.length > 0
+        ? (
+            <div className="container">
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+          )
+         : (
+          <div className="empty">
+             <h2>No movies found</h2> 
+          </div>
+         )
+      }
     </div>
   )
 }
