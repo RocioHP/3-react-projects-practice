61f7774e4cd154a175455bbf2c01a5de1ac4335d
Rocío Hernández <rocioflauta0709@gmail.com>
2024-03-15 22:28:46 +0300

    movieApp css

--- movieApp/src/App.jsx
+++ movieApp/src/App.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com?apikey=9bbba650";
+
 export default function App() {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
