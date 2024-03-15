fb81404e36e38ecbacfa1e72d8dcaed4ee142a59
Rocío Hernández <rocioflauta0709@gmail.com>
2024-03-16 00:56:59 +0300

    movies api working

--- movieApp/src/App.jsx
+++ movieApp/src/App.jsx
@@ -1,8 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
 
 const API_URL = "http://www.omdbapi.com?apikey=9bbba650";
 
+const movie1 = {
+    "Title": "Spiderman",
+    "Year": "1990",
+    "imdbID": "tt0100669",
+    "Type": "movie",
+    "Poster": "N/A"
+};
+
+
 export default function App() {
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -16,8 +30,24 @@ export default function App() {
   }, []);
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input 
+        type="text"
+        placeholder="Search for movies"
+        value="Superman"
+        onChange={() => {}} />
+        <img src={SearchIcon} 
+        alt="search" 
+        onClick={() => {}}
+        />
+      </div>
+
+      <div className="container">
+        <MovieCard movie1={movie1} />
+      </div>
+    </div>
   )
 }
